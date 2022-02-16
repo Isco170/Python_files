2c0a3af143e4c8ad84ad49121cb9039662266810
Francisco Andre <55844517+Isco170@users.noreply.github.com>
2022-02-16 17:52:23 +0200

    Renomear e apagar ficheiro

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+
+# f = open('./files/Homem.pdf', 'w')
+
+# Renomear ficheiro
+# os.rename('./files/Amor.pdf', './files/Eu.pdf')
+
+# Apagar ficheiro
+# os.remove('./files/1.pdf')
+
+# Listar ficheiros
